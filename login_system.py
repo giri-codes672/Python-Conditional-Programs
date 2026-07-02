5f38c2785e2f8289935c14cf5e3c9a3a57539dfa
giri-codes672 <giribabu9059@gmail.com>
2026-07-02 11:47:12 +0530

    Implement simple login system

---
+++ login_system.py
@@ -0,0 +1,12 @@
+# Simple Login System
+
+correct_username = "admin"
+correct_password = "12345"
+
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if username == correct_username and password == correct_password:
+    print("Login Successful")
+else:
+    print("Invalid Username or Password")
